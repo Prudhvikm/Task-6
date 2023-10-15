e036797f557a00364d8a2ff7e37e92a494ba4809
Prudhvikm <145647429+Prudhvikm@users.noreply.github.com>
2023-10-15 15:10:48 +0530

    Add files via upload
    //4.write a class to calculate uber price.

--- Question-4/index.js
+++ Question-4/index.js
@@ -1,1 +1,18 @@
+//4.write a class to calculate uber price.
+class Uber {
+    constructor(BaseFare=40,BookingFee=20,CostPerMile=9,CostPerMinute=5,time=1,mile=2){
 
+    this.BaseFare=BaseFare;
+    this.BookingFee=BookingFee;
+    this.CostPerMile=CostPerMile
+    this.CostPerMinute=CostPerMinute;
+    this.time=time;
+    this.mile=time;
+    }
+    totalPrice(time=this.time , mile=this.mile)
+    {
+        console.log(this.BaseFare)+(this.BookingFee)+(this.CostPerMinute)+(this.time)+(this.mile)
+    }
+}
+let car = new Uber();
+car.totalPrice(40,100); 
