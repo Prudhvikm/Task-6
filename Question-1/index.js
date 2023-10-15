51b5161e5731672d550de86e04c078ccc4ac3c28
Prudhvikm <145647429+Prudhvikm@users.noreply.github.com>
2023-10-15 15:04:36 +0530

    Delete Question-1 directory

--- Question-1/index.js
+++
@@ -1,6 +0,0 @@
-// Question No.1
-// Write a constructor for the class Movie,
-// which takes a String representing the title of the movie, 
-// a String representing the studio, 
-// and a String representing the rating as its arguments,
-// and sets the respective class properties to these values.
